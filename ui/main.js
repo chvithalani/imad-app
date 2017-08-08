ac562710d7855b2722eb4d201fae4d35e2367b93
chvithalani <30662258+chvithalani@users.noreply.github.com>
2017-08-08 16:39:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="E.C. Department, GEC Rajkot";
 var img=document.getElementById('imgchv');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+1;
+    imgchv.style.marginLeft=marginLeft+'px';
+}
 imgchv.onclick=function()
 {
+    var interval=setInterval(moveRight,100);
     imgchv.style.marginLeft='100px'
 }
