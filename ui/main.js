80fc70c275bbf9a96fa5f1f5e7f9d607b8f09625
chvithalani <30662258+chvithalani@users.noreply.github.com>
2017-08-10 15:15:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,17 @@ imgchv.onclick=function()
     var interval=setInterval(moveRight,10);
     imgchv.style.marginLeft='100px'
 }
+//Submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names=['Name1','Name2','Name3'];
+    var list='';
+    for(var i=0;i<names.lengh; i++)
+    {
+        list+='<li>'+names[i]+'</li>'
+    }
+    var ul=document.getElementById('namelist');
+    u.innertHTML=list;
+};
